d94a83606ed9893918ce01930367299da403e6ce
Sokolovafm <castorlandpozitiv@mail.ru>
2022-12-07 20:55:45 +0300

    listviewPlus

---
+++ UP1.1-1.3/WpfApp1/WpfApp1/Pages/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Models;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Pages.AddEditPage());
+            Manager.MainFrame = MainFrame;
+        }
+        /// <summary>
+        /// Обработка нажатия кнопки "назад"
+        /// </summary>
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+        /// <summary>
+        /// Функция отвечает за скрытие кнопки "назад"
+        /// </summary>
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                BtnBack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BtnBack.Visibility = Visibility.Hidden;
+            }
+        }
+        private void ImportTours()
+        {
+            var fileData = File.ReadAllLines(@"C:\users\daimo\uppractice\Tours.txt");
+            var images = Directory.GetFiles(@"C:\users\daimo\uppractice\Tours Photo");
+
+            foreach (var line in fileData)
+            {
+                var data = line.Split('\t');
+
+                var tempTour = new Tour
+                {
+                    Name = data[0].Replace("\"", ""),
+                    TicketCount = int.Parse(data[2]),
+                    Price = decimal.Parse(data[3]),
+                    IsActual = (data[4] == "0") ? false : true
+                };
+
+                foreach (var tourType in data[5].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var currentType = GachiToursEntities.GetContext().Types.ToList().FirstOrDefault(p => p.Name == tourType);
+                    if (currentType != null)
+                    {
+                        tempTour.Types.Add(currentType);
+                    }
+                }
+
+                try
+                {
+                    tempTour.ImagePreview = File.ReadAllBytes(images.FirstOrDefault(p => p.Contains(tempTour.Name)));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                GachiToursEntities.GetContext().Tours.Add(tempTour);
+                GachiToursEntities.GetContext().SaveChanges();
+            }
+
+
+
+        }
+    }
+}
---
+++ UP1.1-1.3/WpfApp1/WpfApp1/Pages/PredprosmotrHotel.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Models;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Interaction logic for PredprosmotrHotel.xaml
+    /// </summary>
+    public partial class PredprosmotrHotel : Page
+    {
+        Models.GachiToursEntities _context = new Models.GachiToursEntities();
+        /// <summary>
+        /// Инициализация и использование функции обновление туров
+        /// </summary>
+        public PredprosmotrHotel()
+        {
+            InitializeComponent();
+
+            var allTypes = GachiToursEntities.GetContext().Types.ToList();
+            allTypes.Insert(0, new Type { Name = "Все типы" });
+            ComboType.ItemsSource = allTypes;
+            CheckActual.IsChecked = true;
+            ComboType.SelectedIndex = 0;
+
+            UpdateTours();
+        }
+        /// <summary>
+        /// Обновление  listveiw туров
+        /// </summary>
+        private void UpdateTours()
+        {
+            var currentTours = GachiToursEntities.GetContext().Tours.ToList();
+            if (ComboType.SelectedIndex > 0)
+            {
+                currentTours = currentTours.Where(p => p.Types.Contains(ComboType.SelectedItem as Type)).ToList();
+            }
+            currentTours = currentTours.Where(p => p.Name.ToLower().Contains(SearchBox.Text.ToLower())).ToList();
+            if (CheckActual.IsChecked.Value)
+            {
+                currentTours = currentTours.Where(p => p.IsActual).ToList();
+            }
+            LVTours.ItemsSource = currentTours.OrderBy(p => p.TicketCount).ToList();
+        }
+
+        /// <summary>
+        /// Функция поиска туров по текстовому боксу
+        /// </summary>
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateTours();
+        }
+
+        /// <summary>
+        /// Поиск по типо отеля
+        /// </summary>
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateTours();
+        }
+        /// <summary>
+        /// Фильтрация по актуальности
+        /// </summary>
+        private void CheckActual_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateTours();
+        }
+    }
+}
