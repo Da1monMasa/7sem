59918b2c7e6c74d90d50e11a45d33bd837e74021
Sokolovafm <castorlandpozitiv@mail.ru>
2022-12-11 15:14:49 +0300

    blagoAndGlazki

---
+++ demoekzam/import data/blagodad/BlagodadMerinov/BlagodadMerinov/Classes/ConnectDBClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerinovBlagodad.Classes
+{
+    internal class ConnectDBClass
+    {
+        public static Models.BlagodatMerinovvvv modelDB;
+
+    }
+}
